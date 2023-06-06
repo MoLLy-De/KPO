5e9b83bd210ffffa5783f9011851db1a797b5776
MoLLy-De <135233566+MoLLy-De@users.noreply.github.com>
2023-06-06 15:32:02 +0500

    Добавлен класс равномерного СВ

--- KPOkursach.py
+++ KPOkursach.py
@@ -32,4 +32,26 @@ class CauchyRandomVariable(RandomVariable):
     def quantile(self, alpha):
         return self.loc + self.scale * math.tan(math.pi * (alpha - 0.5))
 
+class UniformRandomVariable(RandomVariable):
+  def __init__(self, a=0, b=1) -> None:
+    super().__init__()
+    self.a = a
+    self.b = b
+
+  def pdf(self, x):
+    if x >= self.a and x <= self.b:
+      return 1 / (self.b - self.a)
+    else:
+      return 0
+
+  def cdf(self, x):
+    if x <= self.a:
+      return 0
+    elif x >=self.b:
+      return 1
+    else:
+      return (x - self.a) / (self.b - self.a)
+
+  def quantile(self, alpha):
+    return self.a + alpha * (self.b - self.a)
 
